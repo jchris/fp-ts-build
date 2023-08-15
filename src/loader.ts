005abb9f8bd3e0473579675397a0efd747bbeafd
Chris Anderson <jchris@gmail.com>
2023-08-15 14:10:09 -0700

    un crypto changes

--- src/loader.ts
+++ src/loader.ts
@@ -1,67 +1,81 @@
 import { CarReader } from '@ipld/car'
 import { innerMakeCarFile, parseCarFile } from './loader-helpers'
 import { Transaction } from './transaction'
-import type { AnyBlock, AnyLink, BulkResult, CarCommit, DbCarHeader, IdxCarHeader, IdxMetaMap } from './types'
+import type {
+  AnyBlock, AnyCarHeader, AnyLink, BulkResult,
+  CarCommit, DbCarHeader, DbMeta, IdxCarHeader,
+  IdxMeta, IdxMetaMap
+} from './types'
 import { CID } from 'multiformats'
 import { CarStore, HeaderStore } from './store'
+type LoaderOpts = {
+  public?: boolean
+}
 
 abstract class Loader {
   name: string
+  opts: LoaderOpts = {}
+
   headerStore: HeaderStore | undefined
   carStore: CarStore | undefined
   carLog: AnyLink[] = []
   carReaders: Map<string, CarReader> = new Map()
-  ready: Promise<DbCarHeader | IdxCarHeader>
+  ready: Promise<AnyCarHeader>
+  key: string | undefined
+  keyId: string | undefined
 
-  constructor(name: string) {
+  static defaultHeader: AnyCarHeader
+  abstract defaultHeader: AnyCarHeader
+
+  constructor(name: string, opts?: LoaderOpts) {
     this.name = name
-
-    const initializePromise = new Promise<void>((resolve, reject) => {
-      const isBrowser = typeof window !== 'undefined'
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      const module = isBrowser ? require('./store-browser') : require('./store-fs')
-      if (module) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-        this.headerStore = new module.HeaderStore(name) as HeaderStore
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-        this.carStore = new module.CarStore(name) as CarStore
-        resolve()
-      } else {
-        reject(new Error('Failed to initialize stores.'))
-      }
-    })
-
-    this.ready = initializePromise.then(async () => {
+    if (opts) { this.opts = opts }
+    this.ready = this.initializeStores().then(async () => {
       if (!this.headerStore || !this.carStore) throw new Error('stores not initialized')
-      const header = await this.headerStore.load('main')
-      return await this.ingestCarHead(header?.car)
+      const meta = await this.headerStore.load('main')
+      return await this.ingestCarHeadFromMeta(meta)
     })
   }
 
   async commit(t: Transaction, done: IndexerResult | BulkResult, compact: boolean = false): Promise<AnyLink> {
     await this.ready
-    if (!this.headerStore || !this.carStore) throw new Error('stores not initialized')
-    const fp = this.makeCarHeader(t, done, this.carLog, compact)
-    const { cid, bytes } = await innerMakeCarFile(fp, t)
-    await this.carStore.save({ cid, bytes })
+    await this.carStore!.save({ cid, bytes })
     if (compact) {
       for (const cid of this.carLog) {
-        await this.carStore.remove(cid)
+        await this.carStore!.remove(cid)
       }
       this.carLog.splice(0, this.carLog.length, cid)
     } else {
       this.carLog.push(cid)
     }
-    await this.headerStore.save(cid)
+    await this.headerStore!.save({ car: cid, key: this.key || null })
     return cid
   }
 
-  protected abstract makeCarHeader(
-    _t: Transaction,
-    _result: BulkResult | IndexerResult,
-    _cars: AnyLink[],
-    _compact: boolean
-  ): DbCarHeader | IdxCarHeader;
+  async getBlock(cid: CID): Promise<AnyBlock | undefined> {
+    for (const [, reader] of [...this.carReaders]) {
+      const block = await reader.get(cid)
+      if (block) {
+        return block
+      }
+    }
+  }
+
+  protected async initializeStores() {
+    const isBrowser = typeof window !== 'undefined'
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const module = isBrowser ? await require('./store-browser') : await require('./store-fs')
+    if (module) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+      this.headerStore = new module.HeaderStore(this.name) as HeaderStore
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+      this.carStore = new module.CarStore(this.name) as CarStore
+    } else {
+      throw new Error('Failed to initialize stores.')
+    }
+  }
+
+  protected abstract makeCarHeader(_result: BulkResult | IndexerResult, _cars: AnyLink[], _compact: boolean): AnyCarHeader;
 
   protected async loadCar(cid: AnyLink): Promise<CarReader> {
     if (!this.headerStore || !this.carStore) throw new Error('stores not initialized')
@@ -89,53 +103,27 @@ abstract class Loader {
   protected async getMoreReaders(cids: AnyLink[]) {
     await Promise.all(cids.map(cid => this.loadCar(cid)))
   }
-
-  async getBlock(cid: CID): Promise<AnyBlock | undefined> {
-    for (const [, reader] of [...this.carReaders]) {
-      const block = await reader.get(cid)
-      if (block) {
-        return block
-      }
-    }
-  }
-
-  protected abstract defaultCarHeader(): DbCarHeader | IdxCarHeader
 }
 
 export class DbLoader extends Loader {
   declare ready: Promise<DbCarHeader> // todo this will be a map of headers by branch name
 
-  protected makeCarHeader(
-    t: Transaction,
-    result: BulkResult,
-    cars: AnyLink[],
-    compact: boolean = false
-  ): DbCarHeader {
-    const head = result.head
-    if (!head) throw new Error('no head')
-    return compact ? { head, cars: [], compact: cars } : { head, cars, compact: [] }
-  }
+  static defaultHeader = { cars: [], compact: [], head: [] }
+  defaultHeader = DbLoader.defaultHeader
 
-  protected defaultCarHeader(): DbCarHeader {
-    return { head: [], cars: [], compact: [] }
+  protected makeCarHeader({ head }: BulkResult, cars: AnyLink[], compact: boolean = false): DbCarHeader {
+    return compact ? { head, cars: [], compact: cars } : { head, cars, compact: [] }
   }
 }
 
 export class IdxLoader extends Loader {
   declare ready: Promise<IdxCarHeader>
 
-  protected makeCarHeader(
-    t: Transaction,
-    result: IndexerResult,
-    cars: AnyLink[],
-    compact: boolean = false
-  ): IdxCarHeader {
-    const indexes = result.indexes
-    return compact ? { indexes, cars: [], compact: cars } : { indexes, cars, compact: [] }
-  }
+  static defaultHeader = { cars: [], compact: [], indexes: new Map() as Map<string, IdxMeta> }
+  defaultHeader = IdxLoader.defaultHeader
 
-  protected defaultCarHeader(): IdxCarHeader {
-    return { cars: [], compact: [], indexes: new Map() }
+  protected makeCarHeader({ indexes }: IndexerResult, cars: AnyLink[], compact: boolean = false): IdxCarHeader {
+    return compact ? { indexes, cars: [], compact: cars } : { indexes, cars, compact: [] }
   }
 }
 
